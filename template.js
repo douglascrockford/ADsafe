c72ec1e6c8a100543c2c9273541f466fe30f986e
Douglas Crockford <douglas@crockford.com>
2011-01-29 18:57:07 -0800

    first commit

---
+++ template.js
@@ -0,0 +1,19 @@
+// template.js
+
+// ADSAFE.lib is passed a module name and a function that returns a library
+// object that the widget can use.
+
+ADSAFE.lib("template", function () {
+    "use strict";
+
+// This is where the code for the library module is placed. It cannot
+// access the document unless the guest code passes the dom parameter
+// to its methods.
+
+    return {
+
+// Return an object containing the library module's privileged methods.
+// The script can get access to this object at lib.name.
+
+    };
+});
